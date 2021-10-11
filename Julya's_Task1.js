9f7a3999276a14ce41333edd569d81155019a801
Galina <gkukleva734@gmail.com>
2021-10-10 22:48:48 -0600

    First Task

---
+++ Julya's_Task1.js
@@ -0,0 +1,314 @@
+//LESSON 1 HOMEWORK  от Юли с решением
+
+
+//1. Create the variable with the name => first and declare the value 'Hello'
+
+//2. Create the variable with the name => second and declare the value 'World!'
+
+//3. Create the variable with the name => greeting and declare the value of the variable first and second
+
+//4. Print the result of greeting to console
+
+//the result in the end should be "Hello World!"
+
+//===================================
+
+//1. Create the variable with the name => numberOne, and declare the value to 10, Print the result to console
+
+//2. Create the variable with the name => numberTwo, and declare the value to 12, Print the result to console.
+
+//3. Create the variable with the name => sum(сумма), and declare the value of numberOne plus numberTwo, Print the result to console.
+
+//4. Create the variable with the name => subtract(отнять), and declare the result of
+//numberOne minus numberTwo (and reverse numberTwo minus numberOne), Print both results to console.
+
+//5.Do the same with multiplication and division.
+
+//===================================
+
+//1. Create the variable with the name => name, declare the value "John", Print the result to console.
+
+//2. Create the variable with the name => age, declare the value 41, Print the result to console.
+
+
+//3. Create the variable with the name gratings, declare the value of the sum of variables name & age, Print the result to console.
+
+//===================================
+
+// 1. Create the variable with the name a, declare the value of true, resign the value to false. Print the result to console.
+
+//===================================
+
+// 1. Создайте переменные с именами a, b и с, присвойте переменным любые цифровый значения (целые или дробные).
+//С помощью переприсваивания переменных увеличьте a на 2, уменьшите b на 4 и разделите с на 6. Выведите a, b, c в консоль.
+
+
+//===================================
+
+//fixe the code
+//1.Print the result of the sum
+
+//let a = 1,
+//lat b = 2,
+//const result = 0
+//result = a + b;
+//console.log('result');
+
+//2.Print the result of string concatination
+//let 1name = Hello;
+//let 2name = 'World':
+//let str = 1name + name;
+//console.log(str);
+
+//3.Print the result of multiplication
+//let a = 4;
+//let b 2;
+//let multiple = a x b;
+//console.log(multiple);
+
+//===================================
+
+
+//1. Find the perimetr and area of the square. Console the result.
+//let squareSide = 10;
+
+//2. Find the perimetr and area of the rectangle. Console the result.
+//let a = 10;
+//let b = 5;
+
+//1. Crate a variable with name - price and assign the value 5;
+//2. Create a variable with name - amount and assign the value 10;
+//3. Create a variable with name totalPrice an d Calculate the results;
+//4. Console log string 'The total price is {put total price here} for {put amount here} items'
+
+
+//1.
+//let a = 10;
+//let b = 5;
+
+//change the value to a = 5, b = 10;
+
+
+//https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+//https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/javascript
+
+const line = '===================================';
+
+//1.1
+//1. Create the variable with the name => first and declare the value 'Hello'
+
+//2. Create the variable with the name => second and declare the value 'World!'
+
+//3. Create the variable with the name => greeting and declare the value of the variable first and second
+
+//4. Print the result of greeting to console
+
+//the result in the end should be "Hello World!"
+
+let first = 'Hello';
+let second = 'Wolrd!';
+let greeting =first + ' ' + second;
+
+console.log(greeting)
+console.log(line)
+
+//1.2
+//1. Create the variable with the name => numberOne, and declare the value to 10, Print the result to console
+
+//2. Create the variable with the name => numberTwo, and declare the value to 12, Print the result to console.
+
+//3. Create the variable with the name => sum(сумма), and declare the value of numberOne plus numberTwo, Print the result to console.
+
+//4. Create the variable with the name => subtract(отнять), and declare the result of
+//numberOne minus numberTwo (and reverse numberTwo minus numberOne), Print both results to console.
+
+//5.Do the same with multiplication and division.
+
+let numberOne = 10;
+
+console.log(numberOne);
+
+let numberTwo = 12;
+
+console.log(numberTwo);
+
+let sum = numberOne + numberTwo;
+
+console.log(sum);
+
+let subtract = numberOne - numberTwo;
+
+console.log(subtract);
+
+let subtract1 = numberTwo - numberOne;
+
+console.log(subtract1);
+
+let multiplication = numberOne * numberTwo;
+
+console.log(multiplication);
+
+let division = numberOne / numberTwo;
+
+console.log(division);
+console.log(line);
+
+//1.3
+//1. Create the variable with the name => name, declare the value "John", Print the result to console.
+
+//2. Create the variable with the name => age, declare the value 41, Print the result to console.
+
+//3. Create the variable with the name gratings, declare the value of the sum of variables name & age,
+//Print the result to console.
+
+let name = 'John';
+
+console.log(name);
+
+let age = 24;
+
+console.log(age);
+
+let gratings = name + ' ' + age;
+
+console.log(gratings);
+console.log(line);
+
+//1.4
+
+// 1. Create the variable with the name a, declare the value of true, resign the value to false. Print the result to console.
+
+let a = true;
+a = false;
+
+console.log(a);
+console.log(line);
+
+//1.5
+// 1. Создайте переменные с именами a, b и с, присвойте переменным любые цифровый значения (целые или дробные).
+//С помощью переприсваивания переменных увеличьте a на 2, уменьшите b на 4 и разделите с на 6. Выведите a, b, c в консоль.
+
+a = 5;
+let b = 7;
+let c = 24;
+
+a = a + 2;
+b = b - 4;
+c = c / 6;
+
+console.log(a, b, c );
+console.log(line);
+
+//1.6.1
+
+
+//fixe the code
+//1.Print the result of the sum
+
+//let a = 1,
+//lat b = 2,
+//const result = 0
+//result = a + b;
+//console.log('result');
+
+a = 1;
+b = 2;
+let result = 0;
+result = a + b;
+
+console.log(result);
+console.log(line);
+
+//1.6.2
+//2.Print the result of string concatination
+//let 1name = Hello;
+//let 2name = 'World':
+//let str = 1name + name;
+//console.log(str);
+
+let name1 = 'Hello';
+let name2 = 'World';
+let str = name1 + ' ' +name2;
+
+console.log(str);
+console.log(line);
+
+//1.6.3
+//3.Print the result of multiplication
+//let a = 4;
+//let b 2;
+//let multiple = a x b;
+//console.log(multiple);
+
+a = 4;
+b = 2;
+let multiple = a * b;
+
+console.log(multiple);
+console.log(line);
+
+//1.7.1
+//1. Find the perimetr and area of the square. Console the result.
+//let squareSide = 10;
+
+let squareSide = 10;
+let squarePerimetr = squareSide * 4;
+let squareArea = squareSide * squareSide;
+
+console.log(squarePerimetr);
+console.log(squareArea);
+console.log(line);
+
+//1.7.2
+//2. Find the perimetr and area of the rectangle. Console the result.
+//let a = 10;
+//let b = 5;
+
+
+let rectangleSide1 = 10;
+let rectangleSide2 = 5;
+let rectanglePerimetr = (rectangleSide1 + rectangleSide2) * 2;
+let rectangleArea = rectangleSide1^2;
+
+console.log(rectanglePerimetr);
+console.log(rectangleArea);
+console.log(line);
+
+//1.7.3
+
+//1. Crate a variable with name - price and assign the value 5;
+//2. Create a variable with name - amount and assign the value 10;
+//3. Create a variable with name totalPrice an d Calculate the results;
+//4. Console log string 'The total price is {put total price here} for {put amount here} items'
+
+let price = 5;
+let amount = 10;
+let totalPrice = price * amount;
+
+console.log('The total price is ' + totalPrice + ' for ' + amount + ' items');
+console.log(line);
+
+//1.7.4
+//change the value to a = 5, b = 10;
+
+let a1 = 10;
+let b1 = 5;
+
+a1 = a1 + b1;
+b1 = a1 - b1;
+a1 = a1 - b1;
+
+console.log(a1, b1);
+
+
+//https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+
+//https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+//https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/javascript
+
+
+
